cc9093a56ca26d75030cd52f32d8e80ac4e01196
huynhh21 <huynhh21@gmail.com>
2024-03-21 11:57:27 -0500

    Data Entry Page

---
+++ Data Entry/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import DataEntryForm from './DataEntryForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Data Entry Page</h1>
+      <DataEntryForm />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ Data Entry/src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import DataEntryForm from './DataEntryForm';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
